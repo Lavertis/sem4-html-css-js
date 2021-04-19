c4a99b3694929c4e6f4186e42853ed61e41b2a14
Lavertis <29688908+Lavertis@users.noreply.github.com>
2021-04-19 21:17:13 +0200

    feat(lab_6): zadanie 1 i 2

---
+++ lab_6/js/obliczenia.js
@@ -0,0 +1,26 @@
+function calculate() {
+    const installmentInput = document.getElementById("installment");
+    const finalAmountInput = document.getElementById("final-amount");
+    const amount = parseInt(document.getElementById('amount').value);
+    const monthlyInterestRate = parseFloat(document.getElementById("monthly-interest-rate").value);
+    const numberOfInstallments = parseInt(document.getElementById("number-of-installments").value);
+    const numerator = amount * monthlyInterestRate / 100;
+    const denominator = 1 - 1 / Math.pow(1 + monthlyInterestRate / 100, numberOfInstallments);
+    const installment = numerator / denominator;
+    const finalAmount = numberOfInstallments * installment;
+    installmentInput.value = installment.toFixed(2);
+    finalAmountInput.value = finalAmount.toFixed(2);
+}
+
+function updateMonthlyPercentage() {
+    const monthlyInterestRateInput = document.getElementById("monthly-interest-rate");
+    const annualInterestRate = parseFloat(document.getElementById("annual-interest-rate").value);
+    const monthlyInterestRate = annualInterestRate / 12;
+    monthlyInterestRateInput.value = monthlyInterestRate.toFixed(2);
+}
+
+function clearInputs() {
+    const inputsHtmlCollection = document.getElementsByTagName("input");
+    const inputsArray = Array.prototype.slice.call(inputsHtmlCollection);
+    inputsArray.forEach(el => el.value = "");
+}
