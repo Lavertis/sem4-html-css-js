e069f8b680d35c29903e1c77ad308f3534f38c69
Lavertis <29688908+Lavertis@users.noreply.github.com>
2021-05-15 17:59:00 +0200

    fix(lab_11): zadanie 11.3 table

--- lab_11/js/koszyk.js
+++ lab_11/js/koszyk.js
@@ -1,27 +1,34 @@
 window.onload = function () {
 
     document.getElementById("saveBtn").addEventListener("click", function () {
+        const productName = document.getElementById("product-name").value;
+        const productPrice = document.getElementById("product-price").value;
+        const productColour = document.getElementById("product-colour").value;
+        const productQuantity = document.getElementById("product-quantity").value;
+        if ([productName, productPrice, productColour, productQuantity].some(el => el === ""))
+            return;
         let product = {};
-        product.name = document.getElementById("product-name").value;
-        product.price = document.getElementById("product-price").value;
-        product.colour = document.getElementById("product-colour").value;
-        product.quantity = document.getElementById("product-quantity").value;
+        product.name = productName;
+        product.price = productPrice;
+        product.colour = productColour;
+        product.quantity = productQuantity
         localStorage.setItem(`Product ${localStorage.length + 1}`, JSON.stringify(product));
     });
 
     document.getElementById("showBtn").addEventListener("click", function () {
         const basketContainer = document.getElementById("basket-container");
-        let content = "";
+        let content = `<div class="koszyk"><table><tr><th>Nazwa</th><th>Cena</th><th>Kolor</th><th>Liczba sztuk</th></tr>`;
         for (let i = 0; i < localStorage.length; i++) {
             const itemName = localStorage.key(i);
             const item = JSON.parse(localStorage.getItem(itemName));
-            content += `<div>
-                        Nazwa: ${item.name}<br>
-                        Cena: ${item.price}<br>
-                        Kolor: ${item.colour}<br>
-                        Liczba sztuk: ${item.quantity}<br>
-                        </div><br>`
+            content += `<tr>
+                        <td>${item.name}</td>
+                        <td>${item.price}</td>
+                        <td>${item.colour}</td>
+                        <td>${item.quantity}</td>
+                        </tr>`
         }
+        content += `</table></div>`
         basketContainer.innerHTML = content;
     });
 
@@ -29,4 +36,3 @@ window.onload = function () {
         localStorage.clear();
     });
 };
-
