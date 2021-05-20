cfb82d7d433a9ec00a64aa32e143ab4fcfddcb8c
Lavertis <29688908+Lavertis@users.noreply.github.com>
2021-05-20 20:58:02 +0200

    feat(lab_11.2): add error information field & show selected by name while removing in this mode

--- lab_11.1/js/koszyk.js
+++ lab_11.1/js/koszyk.js
@@ -4,6 +4,7 @@ window.onload = function () {
     let productsID = JSON.parse(localStorage.getItem("products")).length;
 
     document.getElementById("saveBtn").addEventListener("click", function () {
+        document.getElementById("error").innerHTML = "";
         let currentProductList = JSON.parse(localStorage.getItem("products"));
         const productName = document.getElementById("product-name").value;
         const productPrice = document.getElementById("product-price").value;
@@ -23,11 +24,13 @@ window.onload = function () {
     });
 
     document.getElementById("showBtn").addEventListener("click", function () {
+        document.getElementById("error").innerHTML = "";
         clearInputs();
         showAllProductsInBasket();
     });
 
     document.getElementById("clearBtn").addEventListener("click", function () {
+        document.getElementById("error").innerHTML = "";
         localStorage.setItem("products", JSON.stringify([]));
         clearBasket();
         clearInputs();
@@ -35,15 +38,22 @@ window.onload = function () {
     });
 
     document.getElementById("searchBtn").addEventListener("click", function () {
+        searchProductByName();
+    });
+
+    function searchProductByName() {
         clearBasket();
+        document.getElementById("error").innerHTML = "";
         document.getElementById("product-price").value = "";
         document.getElementById("product-colour").value = "";
         document.getElementById("product-quantity").value = "";
 
         const productList = JSON.parse(localStorage.getItem("products"));
         const searchedName = document.getElementById("product-name").value;
-        if (localStorage.length === 0 || searchedName === "")
-            return;
+        if (localStorage.length === 0 || searchedName === "") {
+            document.getElementById("error").innerHTML = "Brak produktu o podanej nazwie";
+            return false;
+        }
 
         let products = [];
         for (let i = 0; i < productList.length; i++) {
@@ -54,13 +64,16 @@ window.onload = function () {
 
         if (products.length !== 0)
             showProductArrayInBasket(products);
-    });
+    }
 
     document.getElementById("editBtn").addEventListener("click", function () {
+        document.getElementById("error").innerHTML = "";
         const productName = document.getElementById("product-name").value;
         const productIndex = findProductListIndexByName(productName);
-        if (productIndex === -1)
+        if (productIndex === -1) {
+            document.getElementById("error").innerHTML = "Brak produktu o podanej nazwie";
             return false;
+        }
         let productList = JSON.parse(localStorage.getItem("products"));
         const newProductPrice = document.getElementById("product-price").value;
         const newProductColour = document.getElementById("product-colour").value;
@@ -88,6 +101,10 @@ window.onload = function () {
             if (productAsObj.id === productID) {
                 productList.splice(i, 1);
                 localStorage.setItem("products", JSON.stringify(productList));
+                if (document.getElementById("product-name").value !== "")
+                    searchProductByName();
+                else
+                    showAllProductsInBasket();
                 return true;
             }
         }
@@ -99,7 +116,6 @@ window.onload = function () {
         for (let button of removeButtons) {
             button.addEventListener("click", function () {
                 deleteProductByID(parseInt(button.getAttribute("data-productID")));
-                showAllProductsInBasket();
             });
         }
     }
@@ -114,7 +130,7 @@ window.onload = function () {
                         <td>${productAsObj.price}</td>
                         <td>${productAsObj.colour}</td>
                         <td>${productAsObj.quantity}</td>
-                        <td><button class="removeItemBtn" data-productID="${productAsObj.id}" onclick='usunZadanie("+i+")'>Usuń</button></td>
+                        <td><button class="removeItemBtn" data-productID="${productAsObj.id}">Usuń</button></td>
                         </tr>`;
         }
         content += `</table>`;
